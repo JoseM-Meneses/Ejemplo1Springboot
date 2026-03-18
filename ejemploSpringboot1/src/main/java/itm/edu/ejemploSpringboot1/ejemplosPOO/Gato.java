1b95f0a48a6181d2a7d931fab0198d27e905edac
Jose Meneses <josemeneses1122894@correo.itm.edu.co>
2026-03-17 19:40:38 -0500

    Prueba para pull request

--- ejemploSpringboot1/src/main/java/itm/edu/ejemploSpringboot1/ejemplosPOO/Gato.java
+++ ejemploSpringboot1/src/main/java/itm/edu/ejemploSpringboot1/ejemplosPOO/Gato.java
@@ -6,6 +6,6 @@ public class Gato extends  Animal{
     }
 
     public void hacerSonido(){
-        System.out.println(" gato maullando");
+        System.out.println(" gato maullandoooo");
     }
 }
