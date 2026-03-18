80700ac7e46fbd4c818c0d7f7a5564b1016d39c0
Jose Meneses <josemeneses1122894@correo.itm.edu.co>
2026-03-17 19:16:03 -0500

    Prueba para pull request

--- ejemploSpringboot1/src/main/java/itm/edu/ejemploSpringboot1/ejemplosPOO/Gato.java
+++ ejemploSpringboot1/src/main/java/itm/edu/ejemploSpringboot1/ejemplosPOO/Gato.java
@@ -1,11 +1,11 @@
 package itm.edu.ejemploSpringboot1.ejemplosPOO;
 
-public class Gato extends  Animal{
+public class Gato extends Animal{
     public Gato(String nombre) {
         super(nombre);
     }
 
     public void hacerSonido(){
-        System.out.println(" gato maullando");
+        System.out.println(" el gato con botas hace sonido");
     }
 }
